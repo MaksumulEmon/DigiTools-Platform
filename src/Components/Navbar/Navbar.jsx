0e188e8cedc5c0a9bd99bbe6c173cf91c5844930
MaksumulEmon <maksumulemon300@gmail.com>
2026-03-29 22:49:25 +0600

    Navbar add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,46 +1,64 @@
 import React from 'react';
+import { FaCartShopping } from 'react-icons/fa6';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
     return (
 
-        <div className="navbar bg-base-100  container mx-auto">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
+        <div className='border-b border-[#f2f2f2] border-solid'>
+
+
+            <div className="navbar bg-base-100  container mx-auto ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <GiHamburgerMenu />
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a >Products</a></li>
+                            <li><a>Features</a></li>
+                            <li><a>Pricing</a></li>
+                            <li><a>Testimonials</a></li>
+                            <li><a>FAQ</a></li>
+                            {/* <li>
                             <a>Parent</a>
                             <ul className="p-2">
                                 <li><a>Submenu 1</a></li>
                                 <li><a>Submenu 2</a></li>
                             </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        </li> */}
+
+                        </ul>
+                    </div>
+                    <a className="bg-linear-to-b from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent text-3xl font-extrabold p-3">DigiTools</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Products</a></li>
+
+                        <li><a>Features</a></li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Testimonials</a></li>
+                        <li><a>FAQ</a></li>
                     </ul>
                 </div>
-                <a className="bg-linear-to-b from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent text-3xl font-extrabold p-3">DigiTools</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Products</a></li>
-
-                    <li><a>Features</a></li>
-                    <li><a>Pricing</a></li>
-                    <li><a>Testimonials</a></li>
-                    <li><a>FAQ</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
 
 
-        </div>
+
+                <div className=" navbar-end gap-3 ">
+
+                    <div className=' gap-2 items-center hidden md:flex'>
+                        <FaCartShopping></FaCartShopping>
+                        <li><a className='font-medium'>Login</a></li>
+                    </div>
+
+                    <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">Get Started</a>
+
+                </div>
+            </div>
+        </div >
     );
 };
 
